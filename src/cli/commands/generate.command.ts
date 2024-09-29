fce92356488f9598b0bd0c573fff218c388f6e05
Alexander Sapegin <sapegin04@mail.ru>
2024-09-29 15:06:39 +0500

    get mock data from json server

--- src/cli/commands/generate.command.ts
+++ src/cli/commands/generate.command.ts
@@ -1,12 +1,39 @@
+import got from 'got';
 import { Command } from './command.interface.js';
+import { MockServerData } from '../../shared/models/mock-server-data.model.js';
+import { error } from 'node:console';
 
 export class GenerateCommand implements Command {
+  private mockServerData: MockServerData;
+
   public getName(): string {
     return '--generate';
   }
 
-  public execute(...args: string[]): void {
+  public async execute(...args: string[]): Promise<void> {
     const [count, path, url] = args;
     const offerCount = Number.parseInt(count);
+
+    try {
+      await this.load(url);
+    } catch (error: unknown) {
+      console.error('Can\'t generate data');
+
+      if (error instanceof Error) {
+        console.error(`Details: ${error.message}`)
+      }
+    }
+  }
+
+  private async load(url: string): Promise<void> {
+    try {
+      this.mockServerData = await got.get(url).json();
+    } catch (error: unknown) {
+      if (!(error instanceof Error)) {
+        throw new Error(`Can't load data from ${url}`);
+      }
+
+      throw new Error(`Can't load data from ${url}: ${error.message}`);
+    }
   }
 }
