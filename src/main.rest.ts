97e7c581ed02e2e6102c31db19fcd44364f92e5a
ferdea <sapegin04@mail.ru>
2024-10-17 20:37:22 +0500

    add offer to container

--- src/main.rest.ts
+++ src/main.rest.ts
@@ -4,11 +4,13 @@ import { Application } from './rest/index.js';
 import { Component } from './shared/models/index.js';
 import { createApplicationContainer } from './rest/rest.container.js';
 import { createUserContainer } from './shared/modules/user/user.container.js';
+import { createOfferContainer } from './shared/modules/offer/offer.container.js';
 
 async function bootstrap() {
   const container = Container.merge(
     createApplicationContainer(),
-    createUserContainer()
+    createUserContainer(),
+    createOfferContainer()
   );
 
   const application = container.get<Application>(Component.Application);
